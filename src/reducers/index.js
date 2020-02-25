6cc56fa9930255414b41e0799b5e01574f2d57a9
Sara Ginsburg <Sara.ginsburg.ny@gmail.com>
2020-02-25 09:38:42 -0500

    render list of songs to screen

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -17,7 +17,7 @@ const selectedSongReducer = (selectedSong = null, action) => {
   return selectedSong;
 };
 
-export default reducers = combineReducers({
+export default combineReducers({
   songs: songsReducer,
   selectedSong: selectedSongReducer
 })
